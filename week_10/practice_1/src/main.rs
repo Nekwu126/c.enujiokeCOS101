7b8191c92059c7a0fb23db9bd436e171218b1182
c.enujiokeCOS101 <chienujioke@gmail.com>
2023-12-15 09:03:30 +0100

    (change)

--- week_10/practice_1/src/main.rs
+++ week_10/practice_1/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+   
+   let v = vec![101, 250, 330, 400];
+   //vector v owns the object in the heap
+
+   //only a single variable owns the heap memory at any given time
+   let v2 = 2;
+   //here two variables owns heap value,
+   //two pointers to the same content is not allowed in rust
+
+   //Rust is very smart
 }
